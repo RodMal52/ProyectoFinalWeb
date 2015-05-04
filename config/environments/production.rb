f6b0b577ae2422afaace1a5bf85866c5410d073c
RodMal <rodrigo@skinnystork.com>
2015-05-04 16:52:35 -0500

    Added correct routing to admin panel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {:host => 'eventosteccem.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'eventostec-cem.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings ={
     address: "smtp.gmail.com",
